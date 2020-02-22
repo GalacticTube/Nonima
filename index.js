870d4b73cfd291baa480b5a4ae7d1214b9e8dc77
GalacticTube <51178800+GalacticTube@users.noreply.github.com>
2020-02-22 23:16:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ if(command === 'ping') {
         msg.edit(`Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
     }
 	
+if(command === 'fuck wh') {
+        const msg = await message.channel.send("Fucking...");
+        msg.edit("Fucked");
+    }
+	
     if(command === 'devs') {
 	 const devemb = new Discord.RichEmbed()
 	.setColor('#00ff7f')
