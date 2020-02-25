98761a4b052528d3c8576b92b5029869de40025e
GalacticTube <51178800+GalacticTube@users.noreply.github.com>
2020-02-25 06:33:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,22 +6,12 @@ const ytdl = require("ytdl-core");
 
 const prefix  = process.env.BOT_PREFIX;
 
-const invites = {};
-
 var queue = new Map();
 
-bot.on('ready', () => {
-  // "ready" isn't really ready. We need to wait a spell.
-  wait(1000);
+bot.on("ready", () => {
+    console.log(`I am ready! I am in ${bot.guilds.size} guilds`);
 
-  // Load all invites for all guilds and save them to the cache.
-   bot.guilds.forEach(g => {
-    g.fetchInvites().then(guildInvites => {
-      invites[g.id] = guildInvites;
     bot.user.setActivity(process.env.BOT_STATUS);
-      });
-    });
-  });
 });
 
 bot.on("message", async message => {
@@ -33,21 +23,10 @@ bot.on("message", async message => {
 
     const serverQueue = queue.get(message.guild.id);
 	
-client.on('guildMemberAdd', member => {
-  // To compare, we need to load the current invite list.
-  member.guild.fetchInvites().then(guildInvites => {
-    // This is the *existing* invites for the guild.
-    const ei = invites[member.guild.id];
-    // Update the cached invites for the guild.
-    invites[member.guild.id] = guildInvites;
-    // Look through the invites, find the one for which the uses went up.
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    // This is just to simplify the message being sent below (inviter doesn't have a tag property)
-    const inviter = client.users.get(invite.inviter.id);
-    // Get the log channel (change to your liking)
-    const logChannel = member.guild.channels.find(channel => channel.name === "chat");
-    logChannel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
-  });
+bot.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find(ch => ch.name === 'Welcome');
+  if (!channel) return;
+  channel.send(`Welcome to the server, ${member}`);
 });
 
 if(command === 'ping') {
