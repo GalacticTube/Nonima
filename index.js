a04c900e852a6f35cacc24fd1c65e010769b8094
GalacticTube <51178800+GalacticTube@users.noreply.github.com>
2020-02-26 12:02:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -96,6 +96,18 @@ if(!deleteCount || deleteCount < 2 || deleteCount > 100)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
 
+     if(command === "devpurge") {
+	  if (message.author.id === "572811135305252895") {
+	     const deleteCount = parseInt(args[0], 10);
+    
+if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
+
     if(command === 'kick') {
 	    message.react('✅')
   .then(console.log)
