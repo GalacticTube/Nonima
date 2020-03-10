6dcb4284ac428e5cb82bce8dd9c399d97d675789
GalacticTube <51178800+GalacticTube@users.noreply.github.com>
2020-03-10 19:42:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -100,16 +100,18 @@ if(command === 'ping') {
   }
   
      if(command === "purge") {
-	     message.react('✅')
   .then(console.log)
   .catch(console.error);
+	     message.react('❌')
 	if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply('Sorry you do not have permission!');
 	     const deleteCount = parseInt(args[0], 10);
     
 if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+	message.react('❌')
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
     
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
+	     message.react('❌')
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
